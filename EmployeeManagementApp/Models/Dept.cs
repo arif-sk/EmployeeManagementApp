9154fcc6af7fa225ec1d774b9af596d867b4e41b
unknown <arif.hossain177@gmail.com>
2019-07-16 00:26:32 +0600

    form initiated

---
+++ EmployeeManagementApp/Models/Dept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApp.Models
+{
+    public enum Dept
+    {
+        None,
+        IT,
+        Accounting,
+        HR,
+        Marketing
+    }
+}
